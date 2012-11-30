e7974ccac9abaebedd00959e8417164b44e8c36c
Rob Gordon <rgordon@smumn.edu>
2012-11-30 13:32:32 -0600

    initial commit:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eqserver::Application.config.secret_token = '3b268e33fc8726648bfa7f01ec2d77a5827d727785474f2b117ab324bc1beb3ddcdf99309e067c9fafc3d6f0d4eb22c0928b730abc72355cccc2e6e0a34fea7a'
